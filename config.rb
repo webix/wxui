04c462c7f06d0e813a976d1d42947e35c1b26802
michael <michael@webactually.com>
2015-12-12 03:59:49 +0900

    initializing wxui : building file structure

---
+++ config.rb
@@ -0,0 +1,66 @@
+# Require additional plugins
+require 'compass/import-once/activate'
+require 'sassy-maps'
+require 'susy'
+require 'breakpoint'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+project_path = "./"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+environment = :development #:production #
+
+# You can select your preferred output style here (can be overridden via the command line):
+#output_style = :expanded #or :nested or :compact or :compressed
+output_style = (environment == :production) ? :compressed : :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+# change default encoding
+Encoding.default_external = "UTF-8"
+
+# remove numbers on sprite file name
+module Compass::SassExtensions::Functions::Sprites
+  def sprite_url(map)
+    verify_map(map, "sprite-url")
+    map.generate
+    generated_image_url(Sass::Script::String.new(map.name_and_hash))
+  end
+end
+
+module Compass::SassExtensions::Sprites::SpriteMethods
+  def name_and_hash
+    "#{path}.png"
+  end
+
+  def cleanup_old_sprites
+    Dir[File.join(::Compass.configuration.generated_images_path, "#{path}.png")].each do |file|
+      log :remove, file
+      FileUtils.rm file
+      ::Compass.configuration.run_sprite_removed(file)
+    end
+  end
+end
+
+module Compass
+  class << SpriteImporter
+    def find_all_sprite_map_files(path)
+      glob = "*{#{self::VALID_EXTENSIONS.join(",")}}"
+      Dir.glob(File.join(path, "**", glob))
+    end
+  end
+end
